7d448dc68cb11d4565fdd8444116c0cdc1461e9f
Bahram Behzadian <zodiac_se@yahoo.com>
2017-03-27 14:41:24 -0400

    add Search result but not implement

---
+++ trecCarIndex/src/main/java/RetrievalSys/SearchResult.java
@@ -0,0 +1,14 @@
+package RetrievalSys;
+
+/**
+ * SearchResult.jave -  a class for returning the final search result
+ * Created by behzadb on 3/27/17.
+ *
+ */
+
+
+/**
+ * will be implemented
+ */
+public class SearchResult {
+}
