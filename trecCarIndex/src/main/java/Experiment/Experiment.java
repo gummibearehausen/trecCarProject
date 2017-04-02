715cc87bf55539a53403606842189792eeff8318
Hang Zhang <bryan.zhanghang@gmail.com>
2017-04-02 18:31:44 -0400

    experiment refinement

--- trecCarIndex/src/main/java/Experiment/Experiment.java
+++ trecCarIndex/src/main/java/Experiment/Experiment.java
@@ -1,24 +1,36 @@
 package Experiment;
-import java.io.IOException;
-
-import org.apache.lucene.queryparser.classic.ParseException;
-
 
 import RetrievalSys.*;
 
 public class Experiment {
-	
-	public static void main(String[] arg) throws IOException, ParseException{
-		  String file_dir ="spritzer-v1/";
-		  
+	static int runmodel=1;
+	public static void main(String[] arg) throws Exception{
+		int MeanPrecision_at_k=1000;
+		int Precision_at_k=1000;
+		int hitsPerPage = 1000;
+		double lambda = 0.7;
+		if(runmodel==1){
+		  String file_dir ="spritzer-v1/";		  		  
 		  String outline_name= "spritzer.cbor.outlines";
 		  String qrel_name = "spritzer.cbor.article.qrels";
-		  int hitsPerPage = 10;
-		  
 		  String indexPath = "indexfile/";
 		  Queries Q = new Queries(outline_name,file_dir);
-		  B_Searcher.searchEngine(Q, qrel_name,indexPath,hitsPerPage,file_dir);
-
+		  Searcher.searchEngine(Q, qrel_name,indexPath,hitsPerPage,file_dir,MeanPrecision_at_k,Precision_at_k,lambda);
+		}else if(runmodel==2){
+		  String file_dir2 ="test200/"; 
+		  String outline_name2= "all.test200.cbor.outlines";
+		  String qrel_name2 = "all.test200.cbor.article.qrels";
+		  String indexPath2 ="F:/WikiIndexFile/";		  
+		  Queries Q2= new Queries(outline_name2,file_dir2);
+		  Searcher.searchEngine(Q2, qrel_name2,indexPath2,hitsPerPage,file_dir2,MeanPrecision_at_k,Precision_at_k,lambda);
+		}else{
+			  String file_dir ="spritzer-v1/";		  		  
+			  String outline_name= "spritzer.cbor.outlines";
+			  String qrel_name = "spritzer.cbor.article.qrels";
+			  String indexPath = "indexfile/";
+			  Queries Q = new Queries(outline_name,file_dir);
+			  Searcher.searchEngine(Q, qrel_name,indexPath,hitsPerPage,file_dir,MeanPrecision_at_k,Precision_at_k,lambda);
+		}
 	}
 	  
 
