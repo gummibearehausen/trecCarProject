79c757c48ac17e550e37393e8e33cbbcbfad1407
Tucker <tuckerowens@aol.com>
2017-04-03 17:37:49 -0400

    Adding some files

---
+++ trecCarIndex/src/main/java/entity/EntityDict.java
@@ -0,0 +1,115 @@
+package entity;
+
+
+import java.util.HashMap;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.Arrays;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class EntityDict {
+
+
+  public static void buildIndex(String indexdir, String datafile) throws IOException, ParseException {
+
+      Analyzer analyzer = new StandardAnalyzer();
+      FSDirectory directory = FSDirectory.open(new File(indexdir));
+      IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_1, analyzer);
+      config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+      config.setRAMBufferSizeMB(43);
+      config.setUseCompoundFile(false);
+      final IndexWriter w = new IndexWriter(directory, config);
+
+
+      try ( Stream<String> dStream = Files.lines(Paths.get(datafile)) ) {
+        dStream.map( String::trim )
+               .filter( l -> l.contains("\t") )
+               .map( l -> l.split("\t") )
+               .forEach( s -> {
+                 Document doc = new Document();
+                 doc.add(new StringField("surface_name", s[0], Field.Store.YES));
+                 doc.add(new StringField("entity", s[1], Field.Store.YES));
+                 try { w.addDocument(doc); } catch(Exception e ){}
+               });
+
+      } catch (Exception e){
+      } finally {
+
+        // Close the writer
+        w.close();
+      }
+  }
+
+
+  private IndexSearcher searcher;
+	private Analyzer analyzer;
+	private DirectoryReader dr;
+
+  public EntityDict( String indexdir ) {
+
+    try {
+			dr = DirectoryReader.open( FSDirectory.open( new File(indexdir) ) );
+			searcher = new IndexSearcher(dr);
+		} catch( Exception e ) {
+			throw new IllegalArgumentException("Those parameters don't seem valid");
+		}
+
+
+  }
+
+  public EntityResult lookup( String s ) {
+    Term t = new Term("surface_name", s);
+    Query q = new FuzzyQuery(t);
+    try {
+      TopDocs td = searcher.search(q, 1);
+      for ( ScoreDoc sc : td.scoreDocs  ) {
+        Document d = searcher.doc( sc.doc );
+        return new EntityResult( d.get( "entity" ), sc.score );
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return null;
+  }
+
+
+  public class EntityResult {
+    public String name;
+    public float error;
+    public EntityResult( String name, float error ) {
+      this.name = name;
+      this.error = error;
+    }
+
+    public String toString() {
+      return name + " -> " + error;
+    }
+
+    public EntityGraph.KBNode getKBNode() {
+      return EntityHelper.entityGraph.lookup(name);
+    }
+
+  }
+
+
+
+}
---
+++ trecCarIndex/src/main/java/entity/EntityGraph.java
@@ -0,0 +1,128 @@
+package entity;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+
+public class EntityGraph {
+
+    public static void buildIndex(String indexdir, String datafile) throws IOException, ParseException {
+
+    		Analyzer analyzer = new StandardAnalyzer();
+        FSDirectory directory = FSDirectory.open(new File(indexdir));
+        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_1, analyzer);
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        config.setRAMBufferSizeMB(43);
+        config.setUseCompoundFile(false);
+        final IndexWriter w = new IndexWriter(directory, config);
+
+
+        try ( Stream<String> dStream = Files.lines(Paths.get(datafile)) ) {
+          dStream.map( String::trim )
+                 .filter( l -> l.contains("\t") )
+                 .map( l -> l.split("\t") )
+                 .forEach( s -> {
+                   Document doc = new Document();
+                   doc.add(new StringField("node", s[0], Field.Store.YES));
+                   doc.add(new TextField("edges", String.join("\t", Arrays.copyOfRange(s, 1, s.length)), Field.Store.YES));
+                   try { w.addDocument(doc); } catch(Exception e ){}
+                 });
+
+        } catch (Exception e){
+				} finally {
+
+	        // Close the writer
+	        w.close();
+				}
+    }
+
+
+
+    private IndexSearcher searcher;
+    private Analyzer analyzer;
+    private DirectoryReader dr;
+
+    public EntityGraph( String indexdir ) {
+
+      try {
+        dr = DirectoryReader.open( FSDirectory.open( new File(indexdir) ) );
+        searcher = new IndexSearcher(dr);
+      } catch( Exception e ) {
+        throw new IllegalArgumentException("Those parameters don't seem valid");
+      }
+
+
+    }
+
+    public KBNode lookup( String s ) {
+      Term t = new Term("node", s);
+      Query q = new FuzzyQuery(t);
+      try {
+        TopDocs td = searcher.search(q, 1);
+        for ( ScoreDoc sc : td.scoreDocs  ) {
+          Document d = searcher.doc( sc.doc );
+          return new KBNode( d.get( "node" ), Arrays.asList(d.get("edges").split("\t")) );
+        }
+      } catch (Exception e){
+        throw new RuntimeException(e);
+      }
+      return new KBNode(s, new ArrayList<String>());
+    }
+
+
+    public class KBNode {
+      public String name;
+      private List<String> edges;
+
+      public KBNode( String name, List<String> edges) {
+        this.name = name;
+        this.edges = edges;
+      }
+
+      public String toString() {
+        return name + " " + String.join(" ", edges);
+      }
+
+      public String toShortString() {
+        List<String> neighbors = getNeighborhood().stream()
+                                    .filter( n -> n.edges.size() <= 5 )
+                                    .map(n -> n.toString())
+                                    .collect(Collectors.toList());
+        return name + " " + String.join(" ", neighbors);
+      }
+
+      public List<KBNode> getNeighborhood() {
+          return edges.stream()
+                      .map( e -> EntityGraph.this.lookup(e) )
+                      .collect( Collectors.toList() );
+      }
+
+    }
+
+
+
+}
